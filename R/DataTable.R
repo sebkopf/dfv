1ef48f53c67c5f3d2f212e8f389ed90131304cfa
Sebastian Kopf <skopf@caltech.edu>
2014-01-29 09:50:04 -0800

    DataTable update:  - test function added  - gtk objects moved from widgets into table

--- R/DataTable.R
+++ R/DataTable.R
@@ -3,8 +3,9 @@ DataTable <- setRefClass(
   'DataTable',
   contains = 'GuiElementDataFrame', 
   fields = list(
-    icons = 'list', # keeps track of main icons this GuiElement uses
-    handlers = 'list' # handlers that the gui element needs to keep track of
+    icons = 'list', # keeps track of main icons this GuiElement uses 
+    handlers = 'list', # handlers that the gui element needs to keep track of
+    table = 'list' # holds the model (gtkTreeModel), view (gtkTreeView) and selection (gtkTreeSelection) objects
   ),
   methods = list(
     initialize = function(...) {
@@ -19,82 +20,79 @@ DataTable <- setRefClass(
       #INFO: for more information on this kind of data table layout, look at page 166+ of the R GUI guidebook!
       # topics include styling, how to have filters for columns (serachable --> look on page 172+), multiple selections, tooltips, signals, etc.
       
-      model <- rGtkDataFrame(data) 
-      #test_model <- gtkTreeModelSortNewWithModel(model)
-      view <- gtkTreeView (model)
+      # store data
+      data$frame <<- data
+      
+      table$model <<- rGtkDataFrame(data) 
+      #test_model <- gtkTreeModelSortNewWithModel(table$model)
+      table$view <<- gtkTreeView (table$model)
       
       # styling
-      view['enable-grid-lines'] <- TRUE # grid lines between rows
-      view['rules-hint'] <- TRUE # nice alternating colors
-      view['search-column'] <- FALSE # ctrl-f controlled serach box
-      view['reorderable'] <- TRUE # FIXME: this doesn't actually work properly, not sure why not (there's no drag/drop events really) - should allow vertical drag and drop...
+      table$view['enable-grid-lines'] <<- TRUE # grid lines between rows
+      table$view['rules-hint'] <<- TRUE # nice alternating colors
+      table$view['search-column'] <<- FALSE # ctrl-f controlled serach box
+      table$view['reorderable'] <<- TRUE # FIXME: this doesn't actually work properly, not sure why not (there's no drag/drop events really) - should allow vertical drag and drop...
       
-      selection <- view$getSelection()
-      selection$setMode ("single") # could upgrade this to allow multiple selections!
-      #view['rubber-banding'] <- TRUE # also for multiple selections: 
+      table$selection <<- table$view$getSelection()
+      table$selection$setMode ("single") # could upgrade this to allow multiple selections!
+      #table$view['rubber-banding'] <- TRUE # also for multiple selections: 
       
-      # store objects
-      setData(model = model)
-      setWidgets(table = view)
       
       # add columns
-      mapply(view$insertColumnWithAttributes,
+      mapply(table$view$insertColumnWithAttributes,
                position = -1,
-               title = colnames(model),
-               cell = list(gtkCellRendererText()), text = seq_len(ncol(model)) - 1)
+               title = colnames(table$model),
+               cell = list(gtkCellRendererText()), text = seq_len(ncol(table$model)) - 1)
       
       # enable column sorting by clicking on the header
-      # sapply ( seq_len(ncol (model)), function (i) view$getColumn(i - 1)$setSortColumnId(i - 1))
+      # sapply ( seq_len(ncol (table$model)), function (i) table$view$getColumn(i - 1)$setSortColumnId(i - 1))
       
       # set visibility
-      mapply(function(col, vis) col$setVisible(vis), view$getColumns(), !colnames(model)%in%invisible) # NOTE: this is great! don't need to recreate table, with this can make columns visible/invisible as needed
+      mapply(function(col, vis) col$setVisible(vis), table$view$getColumns(), !colnames(table$model)%in%invisible) # NOTE: this is great! don't need to recreate table, with this can make columns visible/invisible as needed
       
       # selection handler
       if (!is.null(changedHandler))
-        handlers$changedHandler <<- gSignalConnect (selection, "changed" , changedHandler)
+        handlers$changedHandler <<- gSignalConnect (table$selection, "changed" , changedHandler)
       
-      # window (here use the group instead)
-      window <- gtkWindow ( )
-      window$setTitle ( "Tabular view of data frame" ) 
+      # add to parent
       scrolled_window <- gtkScrolledWindow ( ) 
-      window$add ( scrolled_window ) 
-      scrolled_window$add ( view )
+      getToolkitWidget(parent)$add ( scrolled_window ) 
+      scrolled_window$add ( table$view )
       scrolled_window$setPolicy ("automatic", "automatic")
-      
-      mapply(function(col, vis) col$setVisible(vis), view$getColumns(), !colnames(model)%in%invisible)
+      setWidgets(tableGroup = scrolled_window)
     },
     
     addRows = function() {
       #implement me
-      #data$model$appendRows()
+      #table$model$appendRows()
     },
     
     removeRows = function(rows) {
       # implement me - can only be done by replacing the data frame underlying the model!
-      #data$model$setFrame(data.frame minus the rows)
+      #table$model$setFrame(data.frame minus the rows)
     },
     
     #' Get the whole data frame in the table (or parts of it)
-    #' could get the data frame backing the model also via as.data.frame(data$model) but
+    #' could get the data frame backing the model also via as.data.frame(table$model) but
     #' might as well use this one for a little more flexbility
     #' @param rows only supports indices (not rownames, they mean nothing to GTK)
     #' @param columns can be either indices or column names
-    getDataFrame = function (rows, columns) {
+    getTableData = function (rows, columns) {
       if (missing(rows))
-        rows <- seq_len(nrow(data$model)) # return all rows by default
+        rows <- seq_len(nrow(table$model)) # return all rows by default
       if (missing(columns)) 
-        columns <- seq_len(ncol(data$model)) # return all columns by default
+        columns <- seq_len(ncol(table$model)) # return all columns by default
       if (identical(class(columns), 'character'))
-        columns <- which(colnames(data$model)%in%columns)
+        columns <- which(colnames(table$model)%in%columns)
       
-      df <- data.frame(data$model[rows, columns], stringsAsFactors=FALSE)
-      names(df) <- colnames(data$model)[columns]
+      df <- data.frame(table$model[rows, columns], stringsAsFactors=FALSE)
+      names(df) <- colnames(table$model)[columns]
       return(df)
     },
     
     # 'Get the indices of the selected rows
     getSelectedRows = function() {
-      selected_rows <- widgets$table$getSelection()$getSelectedRows()
+      selected_rows <- table$selection$getSelectedRows()
       if ( length ( selected_rows$retval ) ) 
         return(sapply ( selected_rows$retval , gtkTreePathGetIndices ) + 1)
       return (NULL)
@@ -103,7 +101,7 @@ DataTable <- setRefClass(
     # 'Get the values of the selected rows
     getSelectedValues = function(columns){
       if ( !is.null(rows <- getSelectedRows()))
-        return (getDataFrame(rows, columns))
+        return (getTableData(rows, columns))
       return (NULL)
     },
     
@@ -112,26 +110,50 @@ DataTable <- setRefClass(
     selectRows = function(indices, blockHandler = FALSE) {
       # block handler if requested
       if (blockHandler)
-        gSignalHandlerBlock(widgets$table$getSelection(), handlers$changedHandler)
+        gSignalHandlerBlock(table$selection, handlers$changedHandler)
       
       # make selection
-      widgets$table$getSelection()$unselectAll()
+      table$selection$unselectAll()
       for (index in indices)
-          widgets$table$getSelection()$selectPath(gtkTreePathNewFromIndices(index-1))
+          table$selection$selectPath(gtkTreePathNewFromIndices(index-1))
       
       # unblock handler
       if (blockHandler)
-        gSignalHandlerUnblock(widgets$table$getSelection(), handlers$changedHandler)
+        gSignalHandlerUnblock(table$selection, handlers$changedHandler)
     },
 
     selectRowsByValues = function(column, values, blockHandler = FALSE) {
-      indices <- which(getDataFrame(columns = column)[[1]]%in%values)
+      indices <- which(getTableData(columns = column)[[1]]%in%values)
       selectRows(indices, blockHandler = blockHandler)
-    }
+    },
     
-    # FIXME: implement cleanwidgets properly
+    # FIXME: implement destroy GUI properly in base classes
+    destroyGUI = function() {
+      if (!is.null(widgets$tableGroup))
+        gtkWidgetDestroy(widgets$tableGroup) # make sure the table is properly finalized
+      #callSuper()
+    },
+    
+    # 'implement it with testthat package syntax and a modal dialog so user needs to click all buttons maybe?
+    # 'TODO: figure out first exactly how testthat works
+    test = function() {
+      # FIXME: run it as a modal dialog and read back out after user clicked the different buttons what should have happend
+      # FIXME: can I make it click the buttons automatically by itself?
+      
+      options("guiToolkit"="RGtk2")
+      win <- ggroup(horizontal = FALSE, cont=gwindow("DataTable test window"), expand=TRUE)
+      grp <- ggroup(horizontal = TRUE, cont=win)
+      
+      # test implementations
+      gbutton ("Hide column b", cont=grp, handler = function(...) print("test"))
+      gbutton ("Select row a=4", cont=grp, handler = function(...) test$selectRowsByValues('a', 4))
+      gbutton ("Destroy table", cont=grp, handler = function(...) test$destroyGUI())
+      
+      # running the DataTable
+      test <- DataTable$new()
+      test$makeGUI(ggroup(cont = win, expand=TRUE), data.frame(a=1:5, b='test', c='wurst'), invisible = c(), changedHandler = function(...) print(test$getSelectedValues()))
+    }
   )
 )
 
-test <- DataTable$new()
-test$makeGUI(NULL, data.frame(a=1:5, b='test', c='wurst'), invisible = c(), changedHandler = function(...) print(test$getSelectedValues()))
+DataTable$new()$test()
